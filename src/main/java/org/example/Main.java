6ef72f97beb29c8af69878df195fd47d258ba00f
김승규 <rlatmdrb1996@gmail.com>
2024-05-27 10:07:42 +0900

    chore: apply checkstyle hook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,27 +3,14 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-        int param = sc.nextInt();
+    int param = sc.nextInt();
 
-        Calculable expression = new MultiplyExpression();
+    Calculable expression = new MultiplyExpression();
 
-        System.out.printf("my expression's result : %d \n", expression.calculate(param));
-    }
-}
-
-interface Calculable {
-    int calculate(int param);
-}
-
-
-//본인이 작성한 코드를 붙여넣어주세요!
-class MultiplyExpression implements Calculable{
-    @Override
-    public int calculate(int param) {
-        return param * 5;
-    }
+    System.out.printf("my expression's result : %d \n", expression.calculate(param));
+  }
 }
